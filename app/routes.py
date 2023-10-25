9df61580e67e6db44cd2ec98fea17f8f92d58e43
kimathimathiu <kimathimathiu83@gmail.com>
2023-10-25 19:44:46 +0300

    use templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world'
+   """Index URL"""
+   return render_template ('index.html', title="Index")
+
+
+@app.route('/about')
+def about():
+   """Index URL"""
+   return render_template ('about.html', title="About")
